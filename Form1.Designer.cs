a7e192ee4d67a07f1853794f19f6ebec4a588a39
Sajed Nahian <snahian@stuy.edu>
2018-07-20 21:31:16 -0400

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace YoutubeDownloader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.url_label = new System.Windows.Forms.Label();
+            this.videoURL = new System.Windows.Forms.TextBox();
+            this.resolutionComboBox = new System.Windows.Forms.ComboBox();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.downloadButton = new System.Windows.Forms.Button();
+            this.percentageLabel = new System.Windows.Forms.Label();
+            this.fileName_textbox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.errorLabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // url_label
+            // 
+            this.url_label.AutoSize = true;
+            this.url_label.Location = new System.Drawing.Point(31, 15);
+            this.url_label.Name = "url_label";
+            this.url_label.Size = new System.Drawing.Size(32, 13);
+            this.url_label.TabIndex = 0;
+            this.url_label.Text = "URL:";
+            // 
+            // videoURL
+            // 
+            this.videoURL.Location = new System.Drawing.Point(68, 12);
+            this.videoURL.Name = "videoURL";
+            this.videoURL.Size = new System.Drawing.Size(314, 20);
+            this.videoURL.TabIndex = 1;
+            // 
+            // resolutionComboBox
+            // 
+            this.resolutionComboBox.FormattingEnabled = true;
+            this.resolutionComboBox.Items.AddRange(new object[] {
+            "360",
+            "480",
+            "720",
+            "1080"});
+            this.resolutionComboBox.Location = new System.Drawing.Point(68, 38);
+            this.resolutionComboBox.Name = "resolutionComboBox";
+            this.resolutionComboBox.Size = new System.Drawing.Size(121, 21);
+            this.resolutionComboBox.TabIndex = 3;
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(9, 91);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(379, 23);
+            this.progressBar.TabIndex = 4;
+            // 
+            // downloadButton
+            // 
+            this.downloadButton.Location = new System.Drawing.Point(9, 120);
+            this.downloadButton.Name = "downloadButton";
+            this.downloadButton.Size = new System.Drawing.Size(75, 23);
+            this.downloadButton.TabIndex = 6;
+            this.downloadButton.Text = "Download";
+            this.downloadButton.UseVisualStyleBackColor = true;
+            this.downloadButton.Click += new System.EventHandler(this.downloadButton_Click);
+            // 
+            // percentageLabel
+            // 
+            this.percentageLabel.AutoSize = true;
+            this.percentageLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.percentageLabel.Location = new System.Drawing.Point(394, 94);
+            this.percentageLabel.Name = "percentageLabel";
+            this.percentageLabel.Size = new System.Drawing.Size(32, 20);
+            this.percentageLabel.TabIndex = 7;
+            this.percentageLabel.Text = "0%";
+            // 
+            // fileName_textbox
+            // 
+            this.fileName_textbox.Location = new System.Drawing.Point(68, 65);
+            this.fileName_textbox.Name = "fileName_textbox";
+            this.fileName_textbox.Size = new System.Drawing.Size(214, 20);
+            this.fileName_textbox.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Save As:";
+            // 
+            // errorLabel
+            // 
+            this.errorLabel.AutoSize = true;
+            this.errorLabel.ForeColor = System.Drawing.Color.Red;
+            this.errorLabel.Location = new System.Drawing.Point(96, 157);
+            this.errorLabel.Name = "errorLabel";
+            this.errorLabel.Size = new System.Drawing.Size(0, 13);
+            this.errorLabel.TabIndex = 10;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Resolution:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(436, 151);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.errorLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.fileName_textbox);
+            this.Controls.Add(this.percentageLabel);
+            this.Controls.Add(this.downloadButton);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.resolutionComboBox);
+            this.Controls.Add(this.videoURL);
+            this.Controls.Add(this.url_label);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "YoutubeDownloader";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label url_label;
+        private System.Windows.Forms.TextBox videoURL;
+        private System.Windows.Forms.ComboBox resolutionComboBox;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Button downloadButton;
+        private System.Windows.Forms.Label percentageLabel;
+        private System.Windows.Forms.TextBox fileName_textbox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label errorLabel;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using YoutubeExtractor;
+
+namespace YoutubeDownloader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            resolutionComboBox.SelectedIndex = 0;
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+        }
+
+        private void downloadButton_Click(object sender, EventArgs e)
+        {
+            errorLabel.Text = "";
+            progressBar.Minimum = 0;
+            progressBar.Maximum = 100;
+
+            var link = videoURL.Text;
+            IEnumerable<VideoInfo> videoInfos;
+            try
+            {
+                videoInfos = DownloadUrlResolver.GetDownloadUrls(link);
+            }
+            catch
+            {
+                videoURL.Text = "";
+                errorLabel.Text = "Invalid Video URL";
+                return;
+            }
+
+            VideoInfo video;
+            try
+            {
+                video = videoInfos
+                    .First(info =>
+                        info.VideoType == VideoType.Mp4 && info.Resolution == Convert.ToInt32(resolutionComboBox.Text));
+            }
+            catch
+            {
+                errorLabel.Text = "Video doesn't support selected resolution";
+                return;
+            }
+
+            if (video.RequiresDecryption)
+            {
+                DownloadUrlResolver.DecryptDownloadUrl(video);
+            }
+
+            //Console.WriteLine(Path.Combine(Application.StartupPath));
+            var videoDownloader = new VideoDownloader(video, Path.Combine(Application.StartupPath + "/", fileName_textbox.Text + video.VideoExtension));
+
+            videoDownloader.DownloadProgressChanged += VideoDownloaderOnDownloadProgressChanged;
+            videoDownloader.Execute();
+        }
+
+        private void VideoDownloaderOnDownloadProgressChanged(object sender, ProgressEventArgs e)
+        {
+            Invoke(new MethodInvoker(delegate()
+            {
+                progressBar.Value = (int)e.ProgressPercentage;
+                percentageLabel.Text = $"{string.Format("{0:0.##}", e.ProgressPercentage)}%";
+                progressBar.Update();
+            }));
+        }
+    }
+}
